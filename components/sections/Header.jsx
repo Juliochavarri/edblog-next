de081c7aabb7179220ad38dfb3c79122a9f2db6f
Julio Chávarri <juliochavarrid@gmail.com>
2020-08-21 23:44:38 -0500

    Componentes MainMenu y Header

---
+++ components/sections/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainHeader from '../navigation/MainMenu'
+
+const Header = () => (
+    <header>
+        <div>
+            <img src="https://ed.team/static/images/logo-premium.svg" alt="EDlogo"/>
+        </div>
+        <div>
+            <MainHeader />
+        </div>
+    </header>
+)
+
+export default Header
